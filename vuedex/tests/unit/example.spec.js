ea7a689b6d2117fcf51678d08230c5f1c39ea0c3
AydaMontoya <aidamontoya96@gmail.com>
2022-08-17 22:04:24 -0500

    Pruebas unitarias Jest Vue

---
+++ vuedex/tests/unit/example.spec.js
@@ -0,0 +1,16 @@
+import {huevo, espinaca} from  "./jestTest";
+
+describe('Se va a probar la calidad de los ingredientes',() => {
+
+  test('Calidad del huevo', ()  => {
+    expect(huevo().estado[0]).toBe('fresco')
+  });
+
+  test('Calidad de la espinaca', ()  => {
+    expect(espinaca()).toEqual({
+      estado: 1,
+      color: "verde"
+    })
+  });
+
+});
